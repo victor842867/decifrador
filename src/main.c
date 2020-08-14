0cefee7acf48eddf4c8cf5a1ece182adad962788
Victor Ferrari <victorferrari2k7@gmail.com>
2020-08-13 23:04:39 -0300

    lab06 completo

--- src/main.c
+++ src/main.c
@@ -1,20 +1,39 @@
-/* Tarefa de decifrar uma cifra
- *
- * Dica: veja em referencia/cifrador.c o codigo-fonte do programa
- * que gerou as cifras que voce precisa decifrar!
+/* Cifrador Numero 1
  */
 
 #include <stdio.h>
 
-#define TAM_BUFFER 50
+#define TAM_BUFFER 100
 char buffer_entrada[TAM_BUFFER];
 char buffer_saida[TAM_BUFFER];
 
+const char senha[] = "SENHASECRETA";
+
+char soma_modulo_alfabeto(char a, char b) {
+  char a0, b0, c0;
+  if (a>='A' && a<='Z') {
+    a0 = a-'A';
+    b0 = b-'A';
+    c0 = 'A'+((a0-b0)+26)%('Z'-'A'+1);
+    return c0;
+  } else return a;
+}
+
 int main() {
+  int j=0;
+  int i=0;
+
   /* Leitura da entrada */
   fgets(buffer_entrada, TAM_BUFFER, stdin);
 
 
+  for (i=0; (i<TAM_BUFFER) && (buffer_entrada[i]!='\0') && (buffer_entrada[i]!='\n'); i++) {
+
+    buffer_saida[i] = soma_modulo_alfabeto(buffer_entrada[i], senha[j]);
+    j++;
+    if (senha[j]=='\0') j=0;
+  }
+  buffer_saida[i]='\0';
 
   /* Escrever saida na tela */
   printf("%s\n", buffer_saida);
